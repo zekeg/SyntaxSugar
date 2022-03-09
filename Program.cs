fb7572babcf2de129ef57fa3f66805b8ac01803a
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-09 03:21:55 -0600

    changed spacing

--- Program.cs
+++ Program.cs
@@ -10,6 +10,5 @@ internal class SyntaxSugar
         int answer = 4;
         var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         Console.WriteLine(response);
-
     }
 }
