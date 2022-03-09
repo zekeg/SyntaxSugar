394b1ccd37802c81c7b9ce0af9421e223ab3ccd9
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-09 03:20:37 -0600

    initial commit for syntax sugar exercise for true coders

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxSugar;
+
+internal class SyntaxSugar
+{
+
+    static void Main(string[] args)
+    {
+        int answer = 4;
+        var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+        Console.WriteLine(response);
+
+    }
+}
